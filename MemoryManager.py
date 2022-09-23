5328dd283d191a7addbe44de5ebd5029c0dc7c5b
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-09-23 23:21:42 +0300

    Fix 2

--- MemoryManager.py
+++ MemoryManager.py
@@ -9,6 +9,7 @@ class MemoryManager:
         self.max_cells = max_cells
         self.cells_in_row = cells_in_row
         self.memory = ['*' for i in range(self.max_cells)]
+        self.pointers = []
 
     def allocate(self, cells: int) -> int:
         pointer = util.find(self.memory, ['*' for i in range(cells)])
@@ -17,22 +18,31 @@ class MemoryManager:
 
         m = MemoryAllocator.allocate(cells, pointer)
         self.memory = util.replace(self.memory, ['*' for i in range(cells)], m)
+        self.pointers.append((pointer, cells))
         return pointer
 
     def free(self, pointer: int) -> bool:
         if pointer < 0 or pointer > self.max_cells:
             return False
-        self.memory = util.replace(self.memory, self.memory[pointer:pointer + self.cells_in_row], ['*' for i in range(self.cells_in_row)])
-        return True
+        
+        for i in self.pointers:
+            if i[0] == pointer:
+                to_delete = MemoryAllocator.allocate(i[1], i[0])
+                self.memory = util.replace(self.memory, to_delete, ['*' for i in range(i[1])])
+                self.pointers.remove(i)
+                return True
+        return False
 
     def print(self) -> None:
         count = 0
         for i in self.memory:
-            if (i != '*'):
+            if (i == '*'):
+                print('x', end='')
+            else:
                 print(i, end='')
-                count += 1
-                if count == self.cells_in_row:
-                    print()
-                    count = 0
+            count += 1
+            if count == self.cells_in_row:
+                print()
+                count = 0
         print()
         
